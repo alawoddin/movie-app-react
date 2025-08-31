e1e3ac6004801f09f9e81891c8221725ddf52b76
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-09-01 00:27:38 +0430

    the navbar is create

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,27 @@
+import React from 'react';
+import { AppBar, Toolbar, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 const NavBar = () => {
-    return (
-        <div>
-            NavBar Page
-        </div>
-    )
-}
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-export default NavBar
+  return (
+    <>
+      <div>NavBar</div>
+      <AppBar position="fixed">
+        <Toolbar
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            backgroundColor: isMobile ? 'red' : 'blue',
+          }}
+        >
+          {/* toolbar content */}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default NavBar;
